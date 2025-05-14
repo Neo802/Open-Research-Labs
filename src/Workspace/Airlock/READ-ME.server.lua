4b6ca36cca4895802593a08da9240acbf3fb68d0
Albert <93777546+Neo802@users.noreply.github.com>
2025-05-14 14:02:04 +0300

    Update READ-ME.server.lua

--- src/Workspace/Airlock/READ-ME.server.lua
+++ src/Workspace/Airlock/READ-ME.server.lua
@@ -1,9 +1,8 @@
 --// This is a reboot of the Aperture Airlock
 --// UPDATE 29 NOVEMBER 2016: NEW CARD THAT ANIMATES
 --// UPDATE 16 NOVERMBER 2018: CARD SOUNDS
---// Do not use it without permission.
 --[[Permission list:
--GamesCell
+-Everyone under GPL-3.0 License
 --]]
 
 --// Credit goes to cicirici802/cicirici76/GamesCell for the airlock
